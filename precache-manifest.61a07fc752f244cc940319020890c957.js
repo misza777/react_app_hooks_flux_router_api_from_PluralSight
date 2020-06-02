32a7a8f7f0274136318c8475e7447a5ec22a716d
misha novakishvili <46324818+misza777@users.noreply.github.com>
2020-06-02 20:31:57 +0200

    Updates

---
+++ precache-manifest.61a07fc752f244cc940319020890c957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c8a952ee8b0ede2a42e6269225d5181",
+    "url": "/react_app_hooks_flux_router_api_from_PluralSight/index.html"
+  },
+  {
+    "revision": "00cc68de1cd0bb797db6",
+    "url": "/react_app_hooks_flux_router_api_from_PluralSight/static/css/2.d8b55213.chunk.css"
+  },
+  {
+    "revision": "00cc68de1cd0bb797db6",
+    "url": "/react_app_hooks_flux_router_api_from_PluralSight/static/js/2.509adbe3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react_app_hooks_flux_router_api_from_PluralSight/static/js/2.509adbe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fabbf05cb80869398f8",
+    "url": "/react_app_hooks_flux_router_api_from_PluralSight/static/js/main.b83ff8ee.chunk.js"
+  },
+  {
+    "revision": "6fbc32adbc17e198c93c",
+    "url": "/react_app_hooks_flux_router_api_from_PluralSight/static/js/runtime-main.b0013cf7.js"
+  }
+]);
